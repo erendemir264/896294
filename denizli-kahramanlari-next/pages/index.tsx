f9c218d9ff16e53a09005945d00b73ad7643e8b9
Eren DEMİR <31165269+erendemir264@users.noreply.github.com>
2025-04-03 22:37:37 +0300

    Add files via upload

---
+++ denizli-kahramanlari-next/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Denizli'nin Kahramanları</h1>
+      <p>Site başarıyla hazırlandı. Detaylar yakında buraya entegre edilecek.</p>
+    </main>
+  );
+}
